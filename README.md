d3c2a61557bfe7659e60057f2feda4030d812566
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-10-06 19:49:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# FitTrack - Spring Boot Microservice
+
+🚧 **Project under development**
+
+FitTrack is a fitness and activity tracking microservice built with Spring Boot.  
+The project is currently under active development — new features and services will be added soon.
---
+++ back/userservice/src/main/java/com/raxrot/userservice/model/User.java
@@ -0,0 +1,35 @@
+package com.raxrot.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private String firstName;
+    private String lastName;
+    @Enumerated(EnumType.STRING)
+    private UserRole role=UserRole.USER;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
